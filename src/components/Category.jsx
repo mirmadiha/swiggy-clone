7ce0700661b318ad004985be32116522a324037b
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-09-14 23:48:44 +0530

    categories

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoArrowLeft, GoArrowRight } from "react-icons/go";
+import categoryData from '../api/category.json';
+
 function Category(){
+    const [category, setCategory] = useState([]);
+
+    useEffect(() => {
+        setCategory(categoryData);
+    }, []);
     return(
         <div className="ml-45 mr-45 p-[16px] flex justify-between items-center">
            <div>  
@@ -8,9 +15,18 @@ function Category(){
            </div>
 
            <div className="flex items-center flex items-center ">
-            <div className="w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 flex justify-center items-center"><GoArrowLeft /></div>
-            <div className="w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 flex justify-center items-center"><GoArrowRight /></div>
+            <div className="w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 flex justify-center items-center cursor-pointer"><GoArrowLeft /></div>
+            <div className="w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 flex justify-center items-center cursor-pointer"><GoArrowRight /></div>
            </div>
+
+           {category.map((cat, index) => {
+                return (
+                    <div key={index} className="w-[100px] h-[100px]"> 
+                        <img src=""
+                        />
+                    </div>
+                )
+           })}
         </div>
     )
 };
